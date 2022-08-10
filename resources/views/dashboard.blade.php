58850a0b6a9ef49115c3b6f2fd2db20adac60fda
Gepezet <gepezet91@gmail.com>
2022-08-10 21:38:43 +0700

    resolve conflic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@
                             </div>
                         </div>
                         <div class="col-lg-3">
-                            <<<<<<< HEAD=======<div class="card">
+                            <div class="card">
                                 <div class="card-body">
                                     <div class="d-flex flex-row">
                                         <div class="col-3 align-self-center">
@@ -86,86 +86,85 @@
                                             aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="search-type-arrow"></div>
+                                    <div class="d-flex flex-row">
+                                        <div class="col-3 align-self-center">
+                                            <div class="round ">
+                                                <i class="ti-layout-grid3"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-9 align-self-center text-right">
+                                            <div class="m-l-10 ">
+                                                <h5 class="mt-0">{{ $rak }}</h5>
+                                                <p class="mb-0 text-muted">Rak (Penyimpanan)</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="progress mt-3" style="height:3px;">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: 61%;"
+                                            aria-valuenow="61" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-3">
-                        >>>>>>> a6fefac0717cdecf9d9888cea816979a1710a1c2
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="search-type-arrow"></div>
-                                <div class="d-flex flex-row">
-                                    <div class="col-3 align-self-center">
-                                        <div class="round ">
-                                            <i class="ti-layout-grid3"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-9 align-self-center text-right">
-                                        <div class="m-l-10 ">
-                                            <h5 class="mt-0">{{ $rak }}</h5>
-                                            <p class="mb-0 text-muted">Rak (Penyimpanan)</p>
-                                        </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-6">
+                    <div class="card">
+                        <div class="card-body new-user">
+                            <h5 class="header-title mb-4 mt-0">Status Penerimaan</h5>
+                            <div class="d-flex flex-row">
+                                <div class="col-3 align-self-center">
+                                    <div class="round">
+                                        <i class="ti-download"></i>
                                     </div>
                                 </div>
-                                <div class="progress mt-3" style="height:3px;">
-                                    <div class="progress-bar bg-info" role="progressbar" style="width: 61%;"
-                                        aria-valuenow="61" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="col-9 text-right align-self-center">
+                                    <div class="m-l-10 ">
+                                        <h5 class="mt-0">{{ $penerimaan }}</h5>
+                                        <p class="mb-0 text-muted">Penerimaan</p>
+                                    </div>
                                 </div>
+                            </div>
+                            <div class="progress mt-3" style="height:3px;">
+                                <div class="progress-bar bg-secondary" role="progressbar" style="width: 48%;"
+                                    aria-valuenow="48" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                    <div class="card">
+                        <div class="card-body new-user">
+                            <h5 class="header-title mb-4 mt-0">Status Pengiriman</h5>
+                            <div class="d-flex flex-row">
+                                <div class="col-3 align-self-center">
+                                    <div class="round">
+                                        <i class="ti-upload"></i>
+                                    </div>
+                                </div>
+                                <div class="col-9 text-right align-self-center">
+                                    <div class="m-l-10 ">
+                                        <h5 class="mt-0">{{ $pengeluaran }}</h5>
+                                        <p class="mb-0 text-muted">pengeluaran</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="progress mt-3" style="height:3px;">
+                                <div class="progress-bar bg-primary" role="progressbar" style="width: 48%;"
+                                    aria-valuenow="48" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-xl-6">
-                <div class="card">
-                    <div class="card-body new-user">
-                        <h5 class="header-title mb-4 mt-0">Status Penerimaan</h5>
-                        <div class="d-flex flex-row">
-                            <div class="col-3 align-self-center">
-                                <div class="round">
-                                    <i class="ti-download"></i>
-                                </div>
-                            </div>
-                            <div class="col-9 text-right align-self-center">
-                                <div class="m-l-10 ">
-                                    <h5 class="mt-0">{{ $penerimaan }}</h5>
-                                    <p class="mb-0 text-muted">Penerimaan</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="progress mt-3" style="height:3px;">
-                            <div class="progress-bar bg-secondary" role="progressbar" style="width: 48%;"
-                                aria-valuenow="48" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-6">
-                <div class="card">
-                    <div class="card-body new-user">
-                        <h5 class="header-title mb-4 mt-0">Status Pengiriman</h5>
-                        <div class="d-flex flex-row">
-                            <div class="col-3 align-self-center">
-                                <div class="round">
-                                    <i class="ti-upload"></i>
-                                </div>
-                            </div>
-                            <div class="col-9 text-right align-self-center">
-                                <div class="m-l-10 ">
-                                    <h5 class="mt-0">{{ $pengeluaran }}</h5>
-                                    <p class="mb-0 text-muted">pengeluaran</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="progress mt-3" style="height:3px;">
-                            <div class="progress-bar bg-primary" role="progressbar" style="width: 48%;"
-                                aria-valuenow="48" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     </div>
 @endsection
